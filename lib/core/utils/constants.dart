a54602fd02df561130f93f3ba46eb7144dca92fa
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-07-28 15:58:10 +0300

    1. Domain Layer Movie Entities => 1

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,4 @@
+class AppConstance {
+  static const String baseUrl = "https://api.themoviedb.org/3/";
+  static const String apiKey = "232f32895803b83d8683bab61b60814d";
+}
---
+++ lib/movies/domain/entities/movie.dart
@@ -0,0 +1,17 @@
+class Movie {
+  final int id;
+  final String title;
+  final String backdropPath;
+  final List<int> genreIds;
+  final String overview;
+  final double voteAverage;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.overview,
+    required this.voteAverage,
+  });
+}

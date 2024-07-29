160524aeb063cec17ffe4bfb08586f691419bdea
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-07-29 18:56:31 +0300

    8. Data Layer  Movies Repository , speak about Functional programming and Either => 8

--- lib/movies/data/repository/movie_repo_imp.dart
+++ lib/movies/data/repository/movie_repo_imp.dart
@@ -1,22 +1,45 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_app/core/error/exceptions.dart';
+import 'package:movie_app/core/error/failure.dart';
+import 'package:movie_app/movies/data/data%20source/remote_movie_data_source.dart';
 import 'package:movie_app/movies/domain/entities/movie.dart';
 import 'package:movie_app/movies/domain/repository/base_movie_repo.dart';
 
 class MovieRepoImp extends BaseMovieRepo {
+  final BaseRemoteMovieDataSource baseRemoteMovieDataSource;
+
+  MovieRepoImp(this.baseRemoteMovieDataSource);
+
   @override
-  Future<List<Movie>> getNowPlayingMovies() {
-    // TODO: implement getNowPlayingMovies
-    throw UnimplementedError();
+  Future<Either<Failure, List<Movie>>> getNowPlayingMovies() async {
+    final result = await baseRemoteMovieDataSource.getNowPlayingMovies();
+
+    try {
+      return Right(result);
+    } on ServerExceptions catch (e) {
+      return Left(ServerFailure(e.errorMessageModel.statusMessage));
+    }
   }
 
   @override
-  Future<List<Movie>> getPopularMovies() {
-    // TODO: implement getPopularMovies
-    throw UnimplementedError();
+  Future<Either<Failure, List<Movie>>> getPopularMovies() async {
+    final result = await baseRemoteMovieDataSource.getPopularMovies();
+
+    try {
+      return Right(result);
+    } on ServerExceptions catch (e) {
+      return Left(ServerFailure(e.errorMessageModel.statusMessage));
+    }
   }
 
   @override
-  Future<List<Movie>> getTopRatedMovies() {
-    // TODO: implement getTopRatedMovies
-    throw UnimplementedError();
+  Future<Either<Failure, List<Movie>>> getTopRatedMovies() async {
+    final result = await baseRemoteMovieDataSource.getTopRatedMovies();
+
+    try {
+      return Right(result);
+    } on ServerExceptions catch (e) {
+      return Left(ServerFailure(e.errorMessageModel.statusMessage));
+    }
   }
 }

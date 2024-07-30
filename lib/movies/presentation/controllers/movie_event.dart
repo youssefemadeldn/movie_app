87b5dc3e6d977fb2891c667bc7e9799d045a63f3
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-07-31 01:38:57 +0300

    2. Started With Bloc State Management : declare events   => 2.2

--- lib/movies/presentation/controllers/movie_event.dart
+++ lib/movies/presentation/controllers/movie_event.dart
@@ -1,1 +1,13 @@
+import 'package:equatable/equatable.dart';
 
+abstract class MovieEvent extends Equatable {
+  const MovieEvent();
+  @override
+  List<Object?> get props => [];
+}
+
+class GetNowPlayingMoviesEvent extends MovieEvent {}
+
+class GetPopularMoviesEvent extends MovieEvent {}
+
+class GetTopRatedMoviesEvent extends MovieEvent {}

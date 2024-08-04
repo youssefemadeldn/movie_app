f68105e656536deae08ced53a1310c04a8a943ad
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-08-04 23:40:37 +0300

    5. Service locator difference between register lazy singleton and register factory

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -3,24 +3,26 @@ import 'package:movie_app/movies/data/data%20source/remote_movie_data_source.dar
 import 'package:movie_app/movies/data/repository/movie_repo_imp.dart';
 import 'package:movie_app/movies/domain/repository/base_movie_repo.dart';
 import 'package:movie_app/movies/domain/use%20cases/get_now_playing_movies_use_case.dart';
+import 'package:movie_app/movies/presentation/controllers/movie_bloc.dart';
 
 final sl = GetIt.instance;
 
 class ServiceLocator {
   void init() {
-    // BaseRemoteMovieDataSource baseRemoteMovieDataSource =
-    //     RemoteMovieDataSourceImp();
+    // Register RemoteMovieDataSourceImp as BaseRemoteMovieDataSource
     sl.registerLazySingleton<BaseRemoteMovieDataSource>(
         () => RemoteMovieDataSourceImp());
 
-    // BaseMovieRepo baseMovieRepo = MovieRepoImp(baseRemoteMovieDataSource);
+    // Register MovieRepoImp as BaseMovieRepo
+    sl.registerLazySingleton<BaseMovieRepo>(
+        () => MovieRepoImp(sl<BaseRemoteMovieDataSource>()));
 
-    sl.registerLazySingleton<BaseMovieRepo>(() => MovieRepoImp(sl()));
-
-    // final result =
-    //     await GetNowPlayingMoviesUseCase(baseMovieRepo).execute();
-
+    // Register GetNowPlayingMoviesUseCase
     sl.registerLazySingleton<GetNowPlayingMoviesUseCase>(
-        () => GetNowPlayingMoviesUseCase(sl()));
+        () => GetNowPlayingMoviesUseCase(sl<BaseMovieRepo>()));
+
+    // Register GetNowPlayingMoviesUseCase
+    sl.registerFactory(() =>
+        MovieBloc(sl())); // create different object every time you call it
   }
 }
